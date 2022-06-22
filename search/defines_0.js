f03dbae1aa8fe3ea4a11558e87b4459421dad2b6
callummccoll <callummccoll@users.noreply.github.com>
2022-06-22 03:22:46 +0000

    deploy: cffa9705810bfa0ed4d6393c240209cdfe72d57d

--- search/defines_0.js
+++ search/defines_0.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['m_5fpi_0',['M_PI',['../guunits_8c.html#ae71449b1cc6e6250b91f539153a7a0d3',1,'guunits.c']]]
+  ['m_5fpi_0',['M_PI',['../guunits_8c.html#ae71449b1cc6e6250b91f539153a7a0d3',1,'guunits.c']]],
+  ['max_1',['MAX',['../guunits_8c.html#afa99ec4acc4ecb2dc3c2d05da15d0e3f',1,'guunits.c']]],
+  ['min_2',['MIN',['../guunits_8c.html#a3acffbd305ee72dcd4593c0d8af64a4f',1,'guunits.c']]]
 ];
